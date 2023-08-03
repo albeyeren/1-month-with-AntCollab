2d08a7036517046de5c3a526088599ee0b782635
albeyeren <albeyeren.gameonly@gmail.com>
2023-08-03 15:21:58 +0700

    -

--- Day7/soal3/palindrome.js
+++ Day7/soal3/palindrome.js
@@ -1,3 +1,35 @@
+/**
+ * ALGORITMA
+ * 1. Mulai program
+ * 2. Buat variabel 'kata' dalam tipe string yang akan diuji apakah palindrome atau tidak
+ * 3. Buat bariabel 'kataTerbalik' sebagai string kosong yang akan digunakan untuk menyimpan 'kata' yang sudah dialik urutannya
+ * 4. Lakukan perulangan dari akhir 'kata' ke awal (dari karakter terakhir ke karakter pertama)
+ * 5. Setelah perulangan selesai, cek apakah 'kata' sama dengan 'kataTerbalik'
+ * 6. Jika sama cetak true
+ * 7. Jika tidak cetak false
+ * 8. Selesai
+ */
+
+/*
+PSEUDOCODE
+START
+    INPUT kata
+    SET kataTerbalik = ""
+    
+    FOR i = kata.length - 1 
+        kataTerbalik += kata[i]
+    ENDFOR
+
+    IF kata == kataTerbalik THEN
+        DISPLAY true
+    ELSE
+        DISPLAY false
+    ENDIF
+END
+*/
+
+// let kata = "katak" //output: true
+// let kata = "kasur rusak" //output: true
 let kata = "makan";
 let kataTerbalik = '';
 
