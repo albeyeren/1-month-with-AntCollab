ee08bdd927dfcae8a2b32d646ce6e784ee61a931
albeyeren <albeyeren.gameonly@gmail.com>
2023-08-09 12:55:20 +0700

    add Day9 function

---
+++ Day9/function/soal2.js
@@ -0,0 +1,14 @@
+// Tugas 2
+
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari dua parameter yang dikirim.
+
+
+function calculateMultiply(a, b) {
+    return a * b;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);
